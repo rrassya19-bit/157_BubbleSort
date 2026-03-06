3095a542eece48bf051194974e8d20c24a85fd28
rrassya19-bit <rrassys19@gmail.com>
2026-03-06 15:19:00 +0700

    menambahkan Libbary and Looping

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+int a[20];
+int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjan Array: ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Error :Maksimal Input Array Adalah 20\n" << endl;
+        }
+    }
+}
